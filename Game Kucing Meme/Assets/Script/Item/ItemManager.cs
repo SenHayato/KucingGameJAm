1e65d3ad9634601a1ae99ce4c81f649bfec638d2
Ahmad Adillaumam <44931897+wewnumam@users.noreply.github.com>
2024-01-28 06:19:58 +0700

    Build configuration

--- Game Kucing Meme/Assets/Script/Item/ItemManager.cs
+++ Game Kucing Meme/Assets/Script/Item/ItemManager.cs
@@ -1,10 +1,8 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditor.Progress;
 
 public class ItemManager : MonoBehaviour
 {
